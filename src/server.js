f48aa13b2e8907006f2adb249ab83f79fd0bd194
Sadek Alashkar <sa225sd@student.lnu.se>
2026-03-18 23:29:23 +0100

    public middleware decl

--- src/server.js
+++ src/server.js
@@ -10,6 +10,7 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
 
 // View Engine
 app.set('view engine', 'ejs');
