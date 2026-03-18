8e3098ca3999ad852809f23371a455639cad9423
Sadek Alashkar <sa225sd@student.lnu.se>
2026-03-18 01:45:49 +0100

    feat: websocket server instance

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import 'dotenv/config';
 import { homeRouter } from './routes/homeRouter.js';
+import { webhookRouter } from './routes/webhookRouter.js';
+import { wss } from './config/webSocketServer.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -13,9 +15,21 @@ app.use(express.urlencoded({ extended: false }));
 app.set('view engine', 'ejs');
 app.set('views', './src/views');
 
-// Use the home router for all routes starting with /
-app.use('/', homeRouter);
+// Attach the WebSocket server instance to the response object for easy access in controllers
+app.use((req, res, next) => {
+    res.wss = wss;
+    next();
+});
 
-app.listen(PORT, () => {
+app.use('/', homeRouter);
+app.use('/webhook', webhookRouter);
+
+const server = app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
+});
+
+server.on('upgrade', (request, socket, head) => {
+    wss.handleUpgrade(request, socket, head, (socket) => {
+        wss.emit('connection', socket, request);
+    });
 });
