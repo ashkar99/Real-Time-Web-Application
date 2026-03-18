61b207245b7037607afe6f7b0bc3d9eddf0091d8
Sadek Alashkar <sa225sd@student.lnu.se>
2026-03-18 01:05:51 +0100

    delete

--- src/routes/homeRoutes.js
+++
@@ -1,7 +0,0 @@
-
-import express from 'express';
-import { issueController } from '../controllers/issueController.js';
-
-export const homeRouter = express.Router();
-
-homeRouter.get('/', issueController.index);
