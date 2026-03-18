303096bdd365bf27c5b9d15670314cbb683dc378
Sadek Alashkar <sa225sd@student.lnu.se>
2026-03-18 01:41:16 +0100

    feat: webhook router

---
+++ src/routes/webhookRouter.js
@@ -0,0 +1,7 @@
+import express from 'express';
+import { webhookController } from '../controllers/webhookController.js';
+
+export const webhookRouter = express.Router();
+
+// The verifyToken middleware executes BEFORE handleWebhook
+webhookRouter.post('/', webhookController.verifyToken, webhookController.handleWebhook);
