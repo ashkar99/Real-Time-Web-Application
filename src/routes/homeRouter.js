14db5801031bdad9fbe55b1f66f072122736b98c
Sadek Alashkar <sa225sd@student.lnu.se>
2026-03-23 21:17:19 +0100

    UI create issue window

--- src/routes/homeRouter.js
+++ src/routes/homeRouter.js
@@ -7,3 +7,4 @@ export const homeRouter = express.Router();
 homeRouter.get('/', issueController.index);
 
 homeRouter.post('/issues/:iid/close', issueController.closeIssue);
+homeRouter.post('/issues', issueController.createIssue);
