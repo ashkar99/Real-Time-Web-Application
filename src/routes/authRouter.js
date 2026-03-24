6380ada9fcf40149056cf2a6f4b1d67564d38953
Sadek Alashkar <sa225sd@student.lnu.se>
2026-03-24 16:51:50 +0100

    get OAuth callback and exchange token

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -4,3 +4,5 @@ import { authController } from '../controllers/authController.js';
 export const authRouter = express.Router();
 
 authRouter.get('/login', authController.login);
+authRouter.get('/gitlab/callback', authController.callback);
+authRouter.get('/logout', authController.logout);
